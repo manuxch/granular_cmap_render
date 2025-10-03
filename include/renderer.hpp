7a188e60cc37954d6eff6e6362d6f0af41009410
Manuel Carlevaro <manux.ch@gmail.com>
2025-10-03 12:29:40 -0300

    Avance en colorbar

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -1,25 +1,30 @@
 #pragma once
+#include "colormap.hpp"
+#include "grain.hpp"
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
-#include "grain.hpp"
-#include "colormap.hpp"
 
 class Renderer {
 public:
-    Renderer(int width, int height, double margin, double valmin, double valmax);
+  Renderer(int width, int height, double margin, double valmin, double valmax);
 
-    void renderToPNG(const std::string& filename,
-                     const std::vector<std::unique_ptr<Grain>>& grains,
-                     double vmin, double vmax,
-                     double xmin, double xmax, double ymin, double ymax,
-                     const Colormap& cmap);
+  void renderToPNG(const std::string &filename,
+                   const std::vector<std::unique_ptr<Grain>> &grains,
+                   double vmin, double vmax, double xmin, double xmax,
+                   double ymin, double ymax, const Colormap &cmap,
+                   const std::string &cbar_title = "",
+                   const std::string &cbar_unit = "");
+
+  void drawColorbar(cairo_t *cr, double x, double y, double width,
+                    double height, double vmin, double vmax,
+                    const Colormap &cmap, const std::string &title,
+                    const std::string &unit);
 
 private:
-    int width_;
-    int height_;
-    double margin_;
-    double valmin_;
-    double valmax_;
+  int width_;
+  int height_;
+  double margin_;
+  double valmin_;
+  double valmax_;
 };
-
